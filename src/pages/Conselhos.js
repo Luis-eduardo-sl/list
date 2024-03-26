f18294ae7ed01e7072bd5363a6a36f114f217fc8
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-26 17:41:27 -0300

    creating page conselho

---
+++ src/pages/Conselhos.js
@@ -0,0 +1,25 @@
+// import './App.css';
+import MinhaLista from '../components/MinhaLista';
+import { useState } from "react"
+
+
+function Conselhos() {
+const [conselhos, setConselho] = useState([])
+
+const novoConselho = async ()=>{
+  const response= await fetch('https://api.adviceslip.com/advice')
+  const data = await response.json()
+  const conselho = data.slip.advice
+  setConselho([...conselhos, conselho])
+}
+
+  return (
+    <div className="App">
+     <h1>Renderizando Lista</h1>
+     <MinhaLista itens={conselhos} />
+     <button onClick={novoConselho}>Gerar conselho</button>
+    </div>
+  );
+}
+
+export default Conselhos;
