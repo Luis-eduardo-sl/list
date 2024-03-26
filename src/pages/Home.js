df9e77151c4925a50b29528c7969f4d77560e4ba
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-03-26 17:42:29 -0300

    create home

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home (){
+    return(
+        <>
+        <p>Esta é a Home</p>
+        <img src="https://s2-galileu.glbimg.com/9uHHuZrRDHRZsgP3UKqx40thz-E=/0x0:800x450/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_fde5cd494fb04473a83fa5fd57ad4542/internal_photos/bs/2022/U/j/XrUuhZTZKvZEE7KWnglw/e.t.o-extraterrestre-foto-divulgacao-widelg.jpg"/>
+        </>
+    )
+}
+
+export default Home
